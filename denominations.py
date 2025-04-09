d2254f44c90f05f115fadc4eebfe04c762067fb1
Viraaj-ai <vintejvir@gmail.com>
2025-04-09 16:09:07 +0530

    average

---
+++ denominations.py
@@ -0,0 +1,7 @@
+amount=int(input("enter your money:"))
+x=amount//100
+y=(amount%100)//50
+z=((amount%100)%50)//10
+print("number of 100 rupees notes:",x)
+print("number of 50 rupees notes:",y)
+print("number of 10 rupees notes:",z1)
---
+++ percentage.py
@@ -0,0 +1,7 @@
+print("enter your marks for the following subjects")
+maths=int(input("enter your maths marks:"))
+science=int(input("enter your science marks:"))
+english=int(input("enter your english marks:"))
+x=(science+english+maths)
+y=(x/300)*100
+print("percentage of marks obtained by you are:",y)
